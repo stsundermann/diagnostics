206f50312cc2a498100640d86bec4ce76960561a
Stephan Sundermann <s.sundermann@fu-berlin.de>
2019-06-22 21:42:17 +0200

    Use std::isfinite due to deprecation on osx
    On osx the c function finite is deprecated resulting in a build failure because clang fails on deprecated functions by default.

--- diagnostic_updater/include/diagnostic_updater/update_functions.h
+++ diagnostic_updater/include/diagnostic_updater/update_functions.h
@@ -190,7 +190,7 @@ namespace diagnostic_updater
         if (*params_.min_freq_ > 0)
           stat.addf("Minimum acceptable frequency (Hz)", "%f",
               *params_.min_freq_ * (1 - params_.tolerance_));
-        if (finite(*params_.max_freq_))
+        if (std::isfinite(*params_.max_freq_))
           stat.addf("Maximum acceptable frequency (Hz)", "%f",
               *params_.max_freq_ * (1 + params_.tolerance_));
       }
